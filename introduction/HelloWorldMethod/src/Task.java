5dd24a20b1078232ac3fe68f6780fc8c7d399a7c
Martynas <martynas.su@gmail.com>
2020-04-13 16:12:04 +0300

    Added more tasks

---
+++ introduction/HelloWorldMethod/src/Task.java
@@ -0,0 +1,9 @@
+public class Task {
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+
+    public static void printHelloWorld() {
+        System.out.println("Hello world");
+    }
+}
